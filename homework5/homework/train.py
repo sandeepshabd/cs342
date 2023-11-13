31a46c67b82f1e436db4609cc8846b8c3476c9a7
Sandeep Singh <sandeepshabd@gmail.com>
2023-11-13 10:11:00 -0600

    training data code

--- homework5/homework/train.py
+++ homework5/homework/train.py
@@ -1,23 +1,84 @@
 from .planner import Planner, save_model 
 import torch
+from torch import save
+from os import path
 import torch.utils.tensorboard as tb
 import numpy as np
 from .utils import load_data
 from . import dense_transforms
 
 def train(args):
-    from os import path
+
     model = Planner()
     train_logger, valid_logger = None, None
     if args.log_dir is not None:
         train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'))
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-    """
-    Your code here, modify your HW4 code
-    Hint: Use the log function below to debug and visualize your model
-    """
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate, weight_decay=1e-5)
+    
+    import inspect
+    transform = eval(args.transform, {k: v for k, v in inspect.getmembers(dense_transforms) if inspect.isclass(v)})
+    train_loader = load_data('drive_data', num_workers=4, transform=transform)
+    
+    #loss_function = torch.nn.BCEWithLogitsLoss(reduction='none')
+    size_loss_function = torch.nn.MSELoss(reduction='mean')
+    
+    train(model, optimizer, size_loss_function, train_loader, device, args, train_logger)
 
-    save_model(model)
+
+def train(model, optimizer, loss_function, train_data_loader, device, args, logger=None):
+    model = model.to(device)
+
+    if args.continue_training:
+        load_model_state(model, args.model_path)
+
+    global_step = 0
+    for epoch in range(args.num_epochs):
+        model.train()
+        loss_vals = []
+
+        for img, gt_det in train_data_loader:
+            img, gt_det = img.to(device), gt_det.to(device)
+
+            det = model(img)
+            loss_val = loss_function(det, gt_det)
+
+            if logger is not None and global_step % args.log_frequency == 0:
+                log_metrics(logger, img, gt_det, det, loss_val, global_step)
+
+            optimizer.zero_grad()
+            loss_val.backward()
+            optimizer.step()
+            loss_vals.append(loss_val.item())
+            global_step += 1
+
+        avg_loss = sum(loss_vals) / len(loss_vals)
+        print(f'Average loss for epoch {epoch} = {avg_loss}')
+        save_model(model, args.model_path)
+
+def load_model_state(model, model_path):
+    from os import path
+    model.load_state_dict(torch.load(path.join(path.dirname(path.abspath(__file__)), model_path)))
+
+def save_model(model, model_path):
+    from torch import save
+    from os import path
+    save(model.state_dict(), path.join(path.dirname(path.abspath(__file__)), model_path))
+
+def log_metrics(logger, img, gt_det, det, loss_val, global_step):
+    logger.add_images('images', img, global_step)
+    logger.add_images('ground_truth', gt_det, global_step)
+    logger.add_images('detection', det, global_step)
+    logger.add_scalar('loss', loss_val, global_step)
+
+
+def log_metrics(logger, img, gt_det, det, loss_val, global_step):
+    logger.add_images('images', img, global_step)
+    logger.add_images('ground_truth', gt_det, global_step)
+    logger.add_images('detection', det, global_step)
+    logger.add_scalar('loss', loss_val, global_step)
+    
 
 def log(logger, img, label, pred, global_step):
     """
