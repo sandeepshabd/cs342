ffc395248c91fa14a47a213a85a8b1179e34851b
Sandeep Singh <sandeepshabd@gmail.com>
2023-11-12 18:16:08 -0600

    updating rollout method

--- homework5/homework/utils.py
+++ homework5/homework/utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pystk
+import io
+import imageio
 
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms.functional as TF
@@ -65,8 +67,92 @@ class PyTux:
     def _to_image(x, proj, view):
         p = proj @ view @ np.array(list(x) + [1])
         return np.clip(np.array([p[0] / p[-1], -p[1] / p[-1]]), -1, 1)
-
+    
     def rollout(self, track, controller, planner=None, max_frames=1000, verbose=False, data_callback=None):
+
+        if self.k is not None and self.k.config.track == track:
+            self.k.restart()
+            self.k.step()
+        else:
+            if self.k is not None:
+                self.k.stop()
+                del self.k
+            config = pystk.RaceConfig(num_kart=1, laps=1, track=track)
+            config.players[0].controller = pystk.PlayerConfig.Controller.PLAYER_CONTROL
+
+            self.k = pystk.Race(config)
+            self.k.start()
+            self.k.step()
+
+        state = pystk.WorldState()
+        track = pystk.Track()
+
+        last_rescue = 0
+
+        if verbose:
+            import matplotlib.pyplot as plt
+            fig, ax = plt.subplots(1, 1)
+        frames = []
+        for t in range(max_frames):
+
+            state.update()
+            track.update()
+
+            kart = state.players[0].kart
+
+            if np.isclose(kart.overall_distance / track.length, 1.0, atol=2e-3):
+                if verbose:
+                    print("Finished at t=%d" % t)
+                break
+
+            proj = np.array(state.players[0].camera.projection).T
+            view = np.array(state.players[0].camera.view).T
+
+            aim_point_world = self._point_on_track(kart.distance_down_track+TRACK_OFFSET, track)
+            aim_point_image = self._to_image(aim_point_world, proj, view)
+            if data_callback is not None:
+                data_callback(t, np.array(self.k.render_data[0].image), aim_point_image)
+
+            if planner:
+                image = np.array(self.k.render_data[0].image)
+                aim_point_image = planner(TF.to_tensor(image)[None]).squeeze(0).cpu().detach().numpy()
+
+            current_vel = np.linalg.norm(kart.velocity)
+            action = controller(aim_point_image, current_vel)
+
+            if current_vel < 1.0 and t - last_rescue > RESCUE_TIMEOUT:
+                last_rescue = t
+                action.rescue = True
+
+            if verbose:
+                ax.clear()
+                ax.imshow(self.k.render_data[0].image)
+                WH2 = np.array([self.config.screen_width, self.config.screen_height]) / 2
+                ax.add_artist(plt.Circle(WH2*(1+self._to_image(kart.location, proj, view)), 2, ec='b', fill=False, lw=1.5))
+                ax.add_artist(plt.Circle(WH2*(1+self._to_image(aim_point_world, proj, view)), 2, ec='r', fill=False, lw=1.5))
+                if planner:
+                    ap = self._point_on_track(kart.distance_down_track + TRACK_OFFSET, track)
+                    ax.add_artist(plt.Circle(WH2*(1+aim_point_image), 2, ec='g', fill=False, lw=1.5))
+                plt.pause(1e-3)
+                
+                with io.BytesIO() as buff:
+                    fig.savefig(buff,format='raw')
+                    buff.seek(0)
+                    data = np.frombuffer(buff.getvalue(), dtype=np.unit8)
+                w,h = fig.canvas.get_width_height()
+                im = data.reshape((int(h),int(w), -1))
+                
+                frames.append(im)
+
+            self.k.step(action)
+            t += 1
+        
+        if verbose:
+            imageio.mimwrite("test.mp4",frames, fps=30, bitrate=1000000)
+        return t, kart.overall_distance / track.length
+
+
+    def rollout_local(self, track, controller, planner=None, max_frames=1000, verbose=False, data_callback=None):
         """
         Play a level (track) for a single round.
         :param track: Name of the track
