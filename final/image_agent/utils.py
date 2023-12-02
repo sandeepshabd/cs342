107a30564898a421369eb4872b78447515addf08
Sandeep Singh <sandeepshabd@gmail.com>
2023-12-02 10:50:11 -0600

    correcting the return typoe of data read

--- final/image_agent/utils.py
+++ final/image_agent/utils.py
@@ -2,6 +2,9 @@ import os
 import numpy as np
 from . import dense_transforms
 import pystk
+from PIL import Image
+from glob import glob
+from os import path
 
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms.functional as TF
@@ -49,13 +52,12 @@ def center_to_world(ax, ay, iwidth, iheight, proj, height=0.36983):
 
 class SuperTuxDataset(Dataset):
     def __init__(self, dataset_path=DATASET_PATH, transform=dense_transforms.ToTensor()):
-        from PIL import Image
-        from glob import glob
-        from os import path
+
         self.data = []
         for f in glob(path.join(dataset_path, '*.csv')):
             i = Image.open(f.replace('.csv', '.png'))
             i.load()
+            print('------data loaded-------')
             self.data.append((i, np.loadtxt(f, dtype=np.float32, delimiter=',')))
         self.transform = transform
 
@@ -65,8 +67,7 @@ class SuperTuxDataset(Dataset):
     def __getitem__(self, idx):
         data = self.data[idx]
         data = self.transform(*data)
-        image, label = data
-        return image, label
+        return data
 
 
 def load_data(dataset_path=DATASET_PATH, transform=dense_transforms.ToTensor(), num_workers=0, batch_size=128):
