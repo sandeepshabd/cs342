4aa5c401e97b174e918fc00b6162529c509cb7ac
Sandeep Singh <sandeepshabd@gmail.com>
2023-12-03 04:08:50 -0600

    reverting back to planner

--- final/image_agent/planner.py
+++ final/image_agent/planner.py
@@ -1,5 +1,8 @@
+
 import torch
 import torch.nn.functional as F
+
+
 
 def spatial_argmax(logit):
     
@@ -7,65 +10,46 @@ def spatial_argmax(logit):
     return torch.stack(((weights.sum(1) * torch.linspace(-1, 1, logit.size(2)).to(logit.device)[None]).sum(1),
                         (weights.sum(2) * torch.linspace(-1, 1, logit.size(1)).to(logit.device)[None]).sum(1)), 1)
     
-import torch
-import torch.nn as nn
-
-class Planner(nn.Module):
+                        
+class Planner(torch.nn.Module):
     def __init__(self, channels=[16, 32, 64, 32]):
         super().__init__()
 
-        # Define methods for creating convolutional and up-convolutional blocks
-        def conv_block(in_channels, out_channels):
-            return nn.Sequential(
-                nn.BatchNorm2d(in_channels),
-                nn.Conv2d(in_channels, out_channels, kernel_size=5, stride=2, padding=2),
-                nn.ReLU(inplace=True)
-            )
+        conv_block = lambda c, h: [torch.nn.BatchNorm2d(h), torch.nn.Conv2d(h, c, 5, 2, 2), torch.nn.ReLU(True)]
+        upconv_block = lambda c, h: [torch.nn.BatchNorm2d(h), torch.nn.ConvTranspose2d(h, c, 4, 2, 1),
+                                     torch.nn.ReLU(True)]
 
-        def upconv_block(in_channels, out_channels):
-            return nn.Sequential(
-                nn.BatchNorm2d(in_channels),
-                nn.ConvTranspose2d(in_channels, out_channels, kernel_size=4, stride=2, padding=1),
-                nn.ReLU(inplace=True)
-            )
+        h, _conv, _upconv = 3, [], []
+        for c in channels:
+            _conv += conv_block(c, h)
+            h = c
 
-        # Initialize variables
-        in_channels = 3
-        _conv = []
-        _upconv = []
+        for c in channels[:-3:-1]:
+            _upconv += upconv_block(c, h)
+            h = c
 
-        # Create convolutional layers
-        for out_channels in channels:
-            _conv.append(conv_block(in_channels, out_channels))
-            in_channels = out_channels
+        _upconv += [torch.nn.BatchNorm2d(h), torch.nn.Conv2d(h, 1, 1, 1, 0)]
 
-        # Create up-convolutional layers
-        for out_channels in channels[:-3:-1]:
-            _upconv.append(upconv_block(in_channels, out_channels))
-            in_channels = out_channels
-
-        # Add final layer to upconvolution
-        _upconv.append(nn.Sequential(
-            nn.BatchNorm2d(in_channels),
-            nn.Conv2d(in_channels, 1, kernel_size=1)
-        ))
-
-        # Combine layers into sequential models
-        self._conv = nn.Sequential(*_conv)
-        self._upconv = nn.Sequential(*_upconv)
+        self._conv = torch.nn.Sequential(*_conv)
+        self._upconv = torch.nn.Sequential(*_upconv)   
         self._mean = torch.FloatTensor([0.4519, 0.5590, 0.6204])
         self._std = torch.FloatTensor([0.0012, 0.0018, 0.0020])
-                        
+
     def forward(self, img):
-        normalized_img = (img - self._mean.to(img.device)) / self._std.to(img.device)
-        conv_output = self._conv(normalized_img)
-        upconv_output = self._upconv(conv_output)
-        spatial_max = spatial_argmax(upconv_output.squeeze(1))
-        scale = torch.tensor([img.size(3) - 1, img.size(2) - 1], dtype=torch.float32, device=img.device)
-        output = (1 + spatial_max) * scale
-        return output
+        
+        img = (img - self._mean[None, :, None, None].to(img.device)) / self._std[None, :, None, None].to(img.device)
+        h = self._conv(img)
+        x = self._upconv(h)
 
+        output = (1 + spatial_argmax(x.squeeze(1))) 
+        width = img.size(3)
+        height = img.size(2)
+        output = output * torch.as_tensor([width - 1,    height - 1]).float().to(
+            img.device)
 
+        return  output #300/400 range
+
+        #return(x)
 
 def save_model(model):
     from torch import save
@@ -81,4 +65,5 @@ def load_model():
     r = Planner()
     r.load_state_dict(load(path.join(path.dirname(path.abspath(__file__)), 'planner.th'), map_location='cpu'))
     return r
+
 
