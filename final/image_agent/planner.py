ec584ab2dc98c72286c5e391d36b18c41698046d
Sandeep Singh <sandeepshabd@gmail.com>
2023-12-03 05:28:45 -0600

    corecting comma

--- final/image_agent/planner.py
+++ final/image_agent/planner.py
@@ -28,7 +28,7 @@ class Planner(torch.nn.Module):
         self._mean = torch.FloatTensor([0.4519, 0.5590, 0.6204])
         self._std = torch.FloatTensor([0.0012, 0.0018, 0.0020])
         
-        input_channel = 3, 
+        input_channel = 3 
         conv_nw = []
         conv_up =  []
         
