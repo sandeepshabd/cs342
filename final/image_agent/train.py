83934e00e12c419a78e8f4e1a82230143de7fbb6
Sandeep Singh <sandeepshabd@gmail.com>
2023-12-04 15:09:58 -0600

    submission 3

--- final/image_agent/train.py
+++ final/image_agent/train.py
@@ -27,9 +27,8 @@ def train(args):
     if args.continue_training:
         model.load_state_dict(torch.load(path.join(path.dirname(path.abspath(__file__)), 'planner.th')))
 
-    #loss = torch.nn.HuberLoss()
-    loss = torch.nn.CrossEntropyLoss()
-    #loss = torch.nn.L1Loss()
+    #loss = torch.nn.CrossEntropyLoss()
+    loss = torch.nn.L1Loss()
     #loss = torch.nn.MSELoss(reduction='mean') 
 
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-5)
